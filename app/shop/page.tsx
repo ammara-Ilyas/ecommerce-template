550b81f52e5947be3a7769de9b67158a6cc23441
ammara-Ilyas <ismarailyas545@gmail.com>
2024-06-24 20:46:43 +0500

    Shop page created

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import MyProduct from "@/components/mainComponents/productComponent/MyProduct";
-// import SaleSlider from "@/components/slider/SalleSlider";
+import SaleSlider from "@/components/slider/SalleSlider";
 import Sidebar from "@/components/mainComponents/productComponent/Sidebar";
-function page() {
+const page = () => {
   return (
     <div className="flex border-2">
       <div className="w-[25%] border-2 relative h-auto">
         <Sidebar />
       </div>
       <div className="w-[70%] border-2 mr-7">
-        {/* <SaleSlider /> */}
+        <SaleSlider />
         <MyProduct />
       </div>
     </div>
   );
-}
+};
 
 export default page;
