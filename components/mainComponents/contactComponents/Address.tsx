330d4d3df1f747956342b0aadd18bb6c6153466c
ammara-Ilyas <ismarailyas545@gmail.com>
2024-06-27 23:27:20 +0500

    Address component edit

--- components/mainComponents/contactComponents/Address.tsx
+++ components/mainComponents/contactComponents/Address.tsx
@@ -7,39 +7,29 @@ import { FaAddressCard } from "react-icons/fa";
 import Link from "next/link";
 const Addres = () => {
   return (
-    <div className="relative isolate bg-white w-[95%] mx-auto ">
-      <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
-        <div className="relative px-6 pb-10 pt-24 sm:pt-32 lg:static lg:px-8 lg:pt-32 lg:pb-16">
-          <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-[95%] ">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900">
-              Contact Us
-            </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Feel free to reach out to us for any inquiries or assistance.
-              We're here to help!
-            </p>
-            <dl className="mt-10 space-y-4 text-base leading-7 text-gray-600">
-              <div className="flex gap-x-4">
-                <dt className="flex-none">
-                  <MdOutlineMailOutline className="text-3xl" />
-                </dt>
-                <dd>contact@company.com</dd>{" "}
-              </div>
-              <div className="flex gap-x-4">
-                <dt className="flex-none">
-                  <FaAddressCard className="text-3xl" />
-                </dt>
-                <dd>123 Main Street Country</dd>{" "}
-              </div>
-              <div className="mt-8">
-                <dd>
-                  {" "}
-                  <Icon />
-                </dd>
-              </div>
-            </dl>
-          </div>
+    <div className="bg-white w-[95%] mx-auto border-2 flex gap-4">
+      <div className="flex flex-col gap-10 p-10 py-28 ">
+        <h2 className="text-4xl font-bold tracking-tight text-gray-900">
+          Contact Us
+        </h2>
+        <p className="mt-6 text-lg w-[80%] text-gray-600">
+          Feel free to reach out to us for any inquiries or assistance. We're
+          here to help!
+        </p>
+        <div className="flex gap-9  text-gray-600">
+          <span>
+            <MdOutlineMailOutline className="text-3xl" />
+          </span>
+          <span className="text-2xl">contact@hr.com</span>
         </div>
+        <div className="flex gap-9  text-gray-600">
+          <span>
+            <FaAddressCard className="text-2xl" />
+          </span>
+          <span className="text-2xl">123 Main Street Country</span>
+        </div>
+      </div>
+      <div>
         <MessageForm />
       </div>
     </div>
