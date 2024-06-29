c99a0ee8ae1edd3e07473feeea1d58adc21e46e3
ammara-Ilyas <ismarailyas545@gmail.com>
2024-06-29 09:42:53 +0500

    Second section added in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "@/components/mainComponents/aboutComponent/Hero";
+import AboutUs from "@/components/mainComponents/aboutComponent/AboutUs";
 const page = () => {
   return (
     <>
       <Hero />
+      <AboutUs />
     </>
   );
 };
