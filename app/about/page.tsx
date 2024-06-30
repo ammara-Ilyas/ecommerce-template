5d4c4e600ff947b29b47d5ce957b31f63f64e17c
ammara-Ilyas <ismarailyas545@gmail.com>
2024-06-30 09:12:46 +0500

    About page completed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Hero from "@/components/mainComponents/aboutComponent/Hero";
 import AboutUs from "@/components/mainComponents/aboutComponent/AboutUs";
 import NewsLatter from "@/components/mainComponents/contactComponents/Newslatter";
+import ShopWithUs from "@/components/mainComponents/aboutComponent/ShopWithUs";
 const page = () => {
   return (
     <>
       <Hero />
       <AboutUs />
+      <ShopWithUs />
       <NewsLatter />
     </>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 // import HomeSlider from "@/components/slider/HomeSlider";
 import Checkout from "@/components/mainComponents/checkoutComponents/Checkout";
+import ShopWithUs from "@/components/mainComponents/aboutComponent/ShopWithUs";
 export default function Home() {
   return (
     <div>
       {/* <HomeSlider /> */}
       <Checkout />
+      <ShopWithUs />
     </div>
   );
 }
